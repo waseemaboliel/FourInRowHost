e93efd012a7a4b54154f5f621895d6aed44562f1
Waseem Aboliel <waseem.aboliel@gmail.com>
2024-01-08 22:31:08 +0200

    Initial commit

---
+++ Host/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WcfService;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+
+namespace Host
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ServiceHost Host = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            try
+            {
+                Host = new ServiceHost(typeof(ConnectFourService));
+                Host.Description.Behaviors.Add(new ServiceMetadataBehavior { HttpGetEnabled = true });
+                Host.Open();
+                lb.Content = "The server is connected...";
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (Host != null)
+            {
+                Host.Close();
+            }
+        }
+    }
+}
